48b93eae3f3cf133fe45a9456f632ddae140ed1b
pavan_gurazada <35609736+pgurazada@users.noreply.github.com>
2023-08-11 09:46:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ def predict(prompt):
     summary = model(prompt)[0]["summary_text"]
     return summary
 
-textbox = gr.Textbox(placeholder="Enter text to summarize", lines=4)
+textbox = gr.Textbox(placeholder="Enter text to summarize", lines=5)
 iface = gr.Interface(fn=predict, inputs=textbox, outputs="text")
 
 with gr.Blocks() as demo:
